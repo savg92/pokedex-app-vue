097cce81a1c08eb4a3f0c77a975633721937263f
SAVG <1992savg@gmail.com>
2024-04-22 02:52:16 -0300

    Refactor Pokemons.vue component and remove unused import in the code

--- src/pages/Pokemons.vue
+++ src/pages/Pokemons.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	import { ref, computed, watch } from 'vue';
+	import { ref, computed } from 'vue';
 	import { useRoute, useRouter } from 'vue-router';
 	import { useQuery } from '@tanstack/vue-query';
 
